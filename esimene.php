a82b37de455f1cda47af0a0b3bf05fb2de4a9c46
Nikita Mikson <miksniki@users.noreply.github.com>
2017-09-11 11:25:59 +0300

    HTML koodile lisatud esimene php osa

--- esimene.php
+++ esimene.php
@@ -1,3 +1,26 @@
+<?php
+	//Muutujad
+	$myName = "Nikita";
+	$myFamilyName = "Mikson";
+	$practiceStarted = "2017-09-11 8.15";
+	
+	//echo strtotime($practiceStarted);
+	//echo strtotime("now");
+	//$timePassed = strtotime("now") - strtotime($practiceStarted);
+	//echo $timePassed;
+	
+	$hourNow = date("H");
+	$partOfDay = "";
+	
+	//if ($hourNow < 8 {
+	//	$partOfDay = "varane hommik";
+	//}
+	//if ($hourNow >= 8 {
+	//	$partOfDay = "koolipäev";
+	//}
+	
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -12,7 +35,17 @@
 	
 	<?php
 		echo "<p>Täna on vastik ilm!</p>";
+		echo "<p>Täna on ";
+		echo date("d.m.Y");
+		echo ".</p>";
+		echo "<p>Lehe laadimise hetkel oli kell: " .date("H:i:s") ."</p>";
+		echo "Praegu on " .$partOfDaY .".";
 	?>
-	
+	<p>PHP käivitatakse lehe laadimisel ja siis tehakse kogu töö ära. Hiljem, kui vaja midagi 
+		jälle "kalkuleerida", siis laetakse kogu leht uuesti.</p>
+	<?php
+		echo "<p>Lehe autori täisnimi on: " .$myName ." " .$myFamilyName .".</p>";
+	?>
+		
 </body>
 </html>
