b2836ad42c16eeacc0d7c6c0b50b40ae670ae8e9
Nikita Mikson <miksniki@users.noreply.github.com>
2017-09-11 10:37:14 +0300

    HTML koodile lisatud esimene php osa

---
+++ esimene.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>
+		Nikita Miksoni veebiprogemise asjad
+	</title>
+</head>
+<body>
+	<h1>Nikita Mikson</h1>
+	<p>See veebileht on loodud veebiprogrammeerimise kursusel ning ei sisalda mingisugust tõsiseltvõetavat sisu.</p>
+	
+	<?php
+		echo "<p>Täna on vastik ilm!</p>";
+	?>
+	
+</body>
+</html>
